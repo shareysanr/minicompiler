48a33fb5f0587ace003a6e04c08750da8b0083c9
shareysanr <shareysanr1@gmail.com>
2025-01-07 19:39:52 -0500

    Added interpreter files

---
+++ interpreter.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "token.h"
+#include "parser.h"
+#include "interpreter.h"
+
---
+++ interpreter.h
@@ -0,0 +1,10 @@
+#ifndef INTERPRETER_H
+#define INTERPRETER_H
+
+#include "parser.h"
+
+
+int interpret(ASTNode* node);
+
+
+#endif
